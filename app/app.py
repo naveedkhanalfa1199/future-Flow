eb1155d556b45ce5f5a4d5d359daf2440bec2dd7
naveedkhanalfa1199 <n029naveedkhan@gmail.com>
2025-12-31 23:29:13 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,6 +17,8 @@ from reportlab.lib.units import inch
 import os
 import json
 import re
+import requests
+import base64
 from .university_data import (
     PAKISTANI_UNIVERSITIES, ENGLISH_TESTS, FIELD_NAMES,
     COUNTRIES, COUNTRY_LOCATIONS, STUDY_GAPS, CGPA_RANGES,
@@ -36,11 +38,12 @@ app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24).hex()
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=55)
 
 # Email Configuration
-app.config['MAIL_SERVER'] = 'in-v3.mailjet.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAILJET_API_KEY')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAILJET_SECRET_KEY')
+# app.config['MAIL_SERVER'] = 'in-v3.mailjet.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAILJET_API_KEY')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAILJET_SECRET_KEY')
+app.config['MAIL_SUPPRESS_SEND'] = True
 app.config['MAIL_DEFAULT_SENDER'] = 'naveed@studyadvisers.com'
 
 mail = Mail(app)
