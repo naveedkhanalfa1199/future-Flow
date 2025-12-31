236cb00630944af0bb6c6973ba0aef5b12747040
naveedkhanalfa1199 <n029naveedkhan@gmail.com>
2025-12-31 22:23:17 +0500

    Update app.py
    mailjet replaced in app.py SMTP

--- app/app.py
+++ app/app.py
@@ -36,11 +36,11 @@ app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24).hex()
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=55)
 
 # Email Configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'in-v3.mailjet.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'naveed@studyadvisers.com'
-app.config['MAIL_PASSWORD'] = 'qzknzviuzadekffo'  # 16-digit code
+app.config['MAIL_USERNAME'] = os.environ.get('MAILJET_API_KEY')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAILJET_SECRET_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = 'naveed@studyadvisers.com'
 
 mail = Mail(app)
